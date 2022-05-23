e67af6f2463f3a4368c2c2d6f818baef5179140c
daideguchi <dd.1107.11107@gmail.com>
2022-05-23 19:00:41 +0900

    2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -168,19 +168,192 @@ import UIKit
 ////型プロパティ：インスタンスを生成しないで呼び出し可能
 //Dog.barkstatic()
 
-//------
+//------イニシャライザ
 
-//イニシャライザ
-class Dog{
-    var name = ""
-    var age = 0
-    
-    init(name: String, age: Int){
-        self.name = name
-        self.age = age
-    }
-}
+//class Dog{
+//    var name = ""
+//    var age = 0
+//
+//    init(name: String, age: Int){
+//        self.name = name
+//        self.age = age
+//    }
+//}
+//
+//let pochi = Dog(name: "Pochi", age: 5)
+//pochi.name
+//pochi.age
 
-let pochi = Dog(name: "Pochi", age: 5)
-pochi.name
-pochi.age
+//-----クラスの継承
+
+//class Dog{
+//    var name = ""
+//    func bark(){
+//        let message = name + ":Bow-wow"
+//        print(message)
+//    }
+//}
+//
+////クラス名の後に　：　をつけると、後ろのクラスを承継することができる
+//class Shibainu: Dog {
+//    var age = 0
+//    func sayAge(){
+//        let message = name + ": I'm \(age) years old."
+//        print(message)
+//    }
+//}
+//
+////shibainuクラスからtaroインスタンスを生成
+//let taro = Shibainu()
+//taro.name = "Taro"
+//taro.age = 5
+//taro.bark()
+////↓継承したクラスを使用することができている
+//taro.sayAge()
+
+//------構造体(データの保持のみの場合は構造体、それ以外はclassを使うと良い)
+
+//struct Dog{
+// var name = ""
+// var breed = ""
+//}
+//
+//let pochi = Dog(name: "Pochi", breed: "Akitaken")
+//pochi.name
+//pochi.breed
+//
+//struct Cat{
+//    var name: String?
+//    var breed: String?
+//}
+//let tama = Cat(name:"Tama", breed:nil)
+//tama.name
+//tama.breed
+
+//------列挙型(複数の選択肢を利用する場合便利)
+
+//enum Signal{
+//    case Blue
+//    case Yellow
+//    case Red
+//}
+//
+////sigにSignalの値が入る
+//var sig:Signal
+//sig = Signal.Blue
+//
+//switch sig{
+//case .Blue:
+//    print("Go!")
+//case.Yellow:
+//    print("caution!")
+//case.Red:
+//    print("stop!")
+//}
+
+//-----値渡しと参照渡し
+
+//参照渡し:　クラスの場合
+//class Dog{
+//    var age = 0
+//}
+//
+//var pochi = Dog()
+//pochi.age = 5
+//
+//var hachi = pochi
+//hachi.age = 7
+//
+//pochi.age
+//hachi.age
+//
+////値渡し: 構造体の場合
+//struct Cat{
+//    var age = 0
+//}
+//
+//var tama = Cat()
+//tama.age = 5
+//
+//var mike = tama
+//mike.age = 7
+//
+//tama.age
+//mike.age
+//
+////値渡し
+//var a = 5
+//var b = a
+//b = 7
+//a
+//b
+
+//------クロージャ(関数を変数のように扱う)
+//let getAgeMessage = {(name: String, age: Int) -> String in
+//    let message = name + ": I'm \(age) years old."
+//    return message
+//}
+//
+//let result = getAgeMessage("Pochi", 5)
+//print(result)
+//
+//func sayAge(name:String, age: Int, getMessage:(String, Int)-> String){
+//    let message = getMessage(name, age)
+//    print(message)
+//}
+//
+//sayAge(name:"Hachi", age:12 , getMessage: getAgeMessage)
+
+//------プロトコル　規約
+
+//protocol Fish{
+//    var color: String{get set}
+//    func swim()
+//}
+//
+//class Shark: Fish {
+//    var color: String = "Blue"
+//    func swim(){
+//        print("Now quickly swimming...")
+//    }
+//}
+//
+//struct Ray: Fish{
+//    var color: String = "Brown"
+//    func swim(){
+//        print("Now slowly swimming...")
+//
+//    }
+//}
+
+//-----デリゲート
+
+//protocol SardineDelegate: AnyObject{
+//    func didEatSardine()
+//}
+//
+//class Sardine {
+//    weak var delegate: SardineDelegate?
+//    func eaten(){
+//        print("I was eaten...")
+//        //delegateがnilではない場合のみ.以下を実行
+//        delegate?.didEatSardine()
+//    }
+//}
+//
+//class Shark: SardineDelegate{
+//
+//    func eat(){
+//        let sardine = Sardine()
+//        sardine.delegate = self
+//        sardine.eaten()
+//    }
+//
+//
+//    func didEatSardine(){
+//        print("I ate a sardine!")
+//    }
+//}
+//
+//let shark = Shark()
+//shark.eat()
